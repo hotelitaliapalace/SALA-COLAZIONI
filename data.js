4b254ebe215b590028f2a9d66c71bb811ad1385c
pehlivan2022 <99028696+pehlivan2022@users.noreply.github.com>
2025-04-17 05:40:22 +0200

    sasas

---
+++ data.js
@@ -0,0 +1,328 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-breakfast_1",
+      "name": "Breakfast_1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.33101228624344614,
+        "pitch": 0.367750525877403,
+        "fov": 1.3442473497586063
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4342442003419835,
+          "pitch": 0.11637032672863512,
+          "rotation": 0,
+          "target": "1-breakfast_2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-breakfast_2",
+      "name": "Breakfast_2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 3.1073763559502403,
+        "pitch": 0.24697952709450455,
+        "fov": 1.3442473497586063
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3906448122486577,
+          "pitch": 0.08667600146970855,
+          "rotation": 0,
+          "target": "0-breakfast_1"
+        },
+        {
+          "yaw": -1.4999789274471187,
+          "pitch": 0.10245902977586496,
+          "rotation": 0,
+          "target": "2-breakfast_3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.8321443726545255,
+          "pitch": 0.09442822953793062,
+          "title": "Chocolate",
+          "text": "FOUNTAIN"
+        }
+      ]
+    },
+    {
+      "id": "2-breakfast_3",
+      "name": "Breakfast_3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.22312969271688843,
+        "pitch": 0.2790303054197345,
+        "fov": 1.3442473497586063
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9409369561759506,
+          "pitch": 0.14894399220422017,
+          "rotation": 0,
+          "target": "1-breakfast_2"
+        },
+        {
+          "yaw": -1.322469364326679,
+          "pitch": 0.16518254828159584,
+          "rotation": 0,
+          "target": "3-breakfast-with-a-view"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.3171015799812302,
+          "pitch": 0.16974051965329195,
+          "title": "Fresh Crepes &amp; Omelettes",
+          "text": "made by your choice"
+        },
+        {
+          "yaw": 2.3697363304984833,
+          "pitch": 0.13172579425566155,
+          "title": "Freshly sliced",
+          "text": "Italian ham, cheese &amp; more"
+        }
+      ]
+    },
+    {
+      "id": "3-breakfast-with-a-view",
+      "name": "Breakfast with a view",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 2.3057960025066793,
+        "pitch": 0.19248416600219187,
+        "fov": 1.3442473497586063
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.004831613501036003,
+          "pitch": 0.03646690836191091,
+          "rotation": 0,
+          "target": "0-breakfast_1"
+        },
+        {
+          "yaw": 2.7996954700097163,
+          "pitch": 0.1373475997273541,
+          "rotation": 0,
+          "target": "4-panoramic-terrace"
+        },
+        {
+          "yaw": 3.0554917224460745,
+          "pitch": 0.05060115926668729,
+          "rotation": 0,
+          "target": "5-voliera"
+        },
+        {
+          "yaw": 0.682076569560639,
+          "pitch": 0.19498480374425853,
+          "rotation": 0,
+          "target": "1-breakfast_2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.6225442958609761,
+          "pitch": 0.22264960442594628,
+          "title": "SWEET",
+          "text": "HOMEMADE CAKES &amp; MORE"
+        }
+      ]
+    },
+    {
+      "id": "4-panoramic-terrace",
+      "name": "Panoramic Terrace",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -2.578841767837421,
+        "pitch": 0.2771449655182572,
+        "fov": 1.3442473497586063
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8280600897483037,
+          "pitch": 0.09057832104774022,
+          "rotation": 0,
+          "target": "2-breakfast_3"
+        },
+        {
+          "yaw": -2.9982903740313827,
+          "pitch": 0.05365857913237804,
+          "rotation": 0,
+          "target": "0-breakfast_1"
+        },
+        {
+          "yaw": -0.130164653803563,
+          "pitch": 0.12703417088721736,
+          "rotation": 0,
+          "target": "5-voliera"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-voliera",
+      "name": "Voliera",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.42726209250209735,
+        "pitch": 0.18814365179459358,
+        "fov": 1.3442473497586063
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8455327153995906,
+          "pitch": 0.14289549155973624,
+          "rotation": 0,
+          "target": "0-breakfast_1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Breakfast Terrace_2",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
